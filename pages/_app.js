c41cf28b6179fded6b6bbeb0a3887678c1c13d87
unknown <cristhianeleden@gamil.com>
2024-01-15 23:20:15 -0500

    [Fix] analytics

--- pages/_app.js
+++ pages/_app.js
@@ -35,9 +35,7 @@ export default function App({ Component, pageProps }) {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
                 `}
       </Script>
       <RLSProvider
