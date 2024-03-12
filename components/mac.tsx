6c2ee69ae3e8eb785f3ba11a437fc703ea75790a
unknown <cristhianeleden@gamil.com>
2024-03-12 11:38:36 -0500

    [Feat] 3d mac

--- components/mac.tsx
+++ components/mac.tsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useId } from 'react'
+import React, { useEffect, useId } from 'react';
 import { Application } from '@splinetool/runtime';
 
 const Mac = () => {
-    const id = useId();
+  const id = useId();
 
-    useEffect(() => {
-        const canvas = document.getElementById(id) as HTMLCanvasElement;
-        const app = new Application(canvas);
-        app.load('https://prod.spline.design/pyznar0hkU6Xpxqi/scene.splinecode');
-    }, [])
-    return (
-        <canvas onMouseEnter={() => {document.body.style.overflowY = "hidden"}} onMouseLeave={() => {document.body.style.overflowY = "auto"}} id={id} ></canvas>
-    )
-}
+  useEffect(() => {
+    const canvas = document.getElementById(id) as HTMLCanvasElement;
+    const app = new Application(canvas);
+    app.load('https://prod.spline.design/pyznar0hkU6Xpxqi/scene.splinecode');
+  }, []);
+  return (
+    <canvas
+      onMouseEnter={() => {
+        document.body.style.overflowY = 'hidden';
+      }}
+      onMouseLeave={() => {
+        document.body.style.overflowY = 'auto';
+      }}
+      id={id}></canvas>
+  );
+};
 
-export default Mac
+export default Mac;
